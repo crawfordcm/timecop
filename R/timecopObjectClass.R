91e8b6a5ecc957c2261b5d8fe025a70b7f1ea4c7
Christopher Crawford <47258177+crawfordcm@users.noreply.github.com>
2025-07-08 16:14:03 -0400

    Update object construction function

--- R/timecopObjectClass.R
+++ R/timecopObjectClass.R
@@ -8,16 +8,6 @@ check_timecop <- function(object) {
     errors <- c(errors, msg)
   }
 
-  if (length(family) != d) {
-    msg    <- c("timecop error: number of families must match number of variables")
-    errors <- c(errors, msg)
-  }
-
-  if (p != 1) {
-    msg    <- c("timecrop error: only lag 1 is currently supported")
-    errors <- c(errors, msg)
-  }
-
   if (length(errors) == 0) TRUE else errors
 
 }
@@ -31,9 +21,10 @@ check_timecop <- function(object) {
 setClass(
   Class = "timecop",
   slots = list(
-    gamma_hat = "matrix", # lag 1 matrix (if p = 1)
-    Gamma_hat = "matrix", # lag 0 matrix (if p = 1)
-    Cov_X_hat = "matrx",
+    cov_z_hat = "array",
+    #gamma_hat = "matrix", # lag 1 matrix (if p = 1)
+    #Gamma_hat = "matrix", # lag 0 matrix (if p = 1)
+    cov_x_hat = "array",
     d = "numeric",
     p = "numeric",
     n = "numeric",
@@ -75,10 +66,72 @@ timecop <- function(data = NULL,
     grid_u <- grid
   }
 
+  # compute observed and latent covariances
   cov_x_hat  <- observed_var_cov(data, d, p, n)
   ell_ij_hat <- latent_var_link(data, d, n, k, family)
   cov_z_hat  <- latent_var_invlink(cov_x_hat, d, p, ell_ij_hat, grid_u)
 
+  # construct implied latent covariances
+  gamma_hat <- array(NA,dim=c(p*d,d))
+  for (h in 1:p) {
+    gamma_hat[((h-1)*d+1):(h*d),] <- cov_z_hat[,,(p+1-h)]
+  }
+
+  Gamma_hat <- array(NA,dim=c(p*d,p*d))
+  for (i in 1:p) {
+    for (j in 1:p) {
+      if (i < j) {
+        Gamma_hat[((i-1)*d+1):(i*d),((j-1)*d+1):(j*d)] <- cov_z_hat[,,(p+1+abs(i-j))]
+      }else if (i == j) {
+        Gamma_hat[((i-1)*d+1):(i*d),((j-1)*d+1):(j*d)] <- cov_z_hat[,,(p+1)]
+      } else { # i > j
+        Gamma_hat[((i-1)*d+1):(i*d),((j-1)*d+1):(j*d)] <- cov_z_hat[,,(p+1-abs(i-j))]
+      }
+    }
+  }
+
+  # ensure that the interpolation didn't compute impossible values
+  gamma_hat[gamma_hat > 1] <- 1
+  gamma_hat[gamma_hat < -1] <- -1
+  Gamma_hat[Gamma_hat > 1] <- 1
+  Gamma_hat[Gamma_hat < -1] <- -1
+
+  # check positive definiteness of Gamma_hat
+  # this is inelegant: could be better
+  pd_approx <- !is.positive.definite(Gamma_hat)
+
+  if (pd_approx) {
+    eig.tol  <- 1e-6
+    posd.tol <- 1e-6
+    pd_mat <- nearPD(Gamma_hat, corr = TRUE, keepDiag = TRUE, eig.tol = eig.tol, posd.tol = posd.tol)
+    Gamma_hat <- as.matrix(pd_mat$mat)
+
+    while (any(abs(solve(Gamma_hat)) > 5)) {
+      eig.tol <- eig.tol * 10
+      posd.tol <- posd.tol * 10
+      pd_mat <- nearPD(Gamma_hat, corr = TRUE, keepDiag = TRUE, eig.tol = eig.tol, posd.tol = posd.tol)
+      Gamma_hat <- as.matrix(pd_mat$mat)
+    }
+  }
+
+  obj <- new("timecop",
+             cov_z_hat = cov_z_hat,
+             cov_x_hat = cov_x_hat,
+             d = d,
+             p = p,
+             n = n,
+             family = family)
+
+  return(obj)
+
 }
+
+#' Fit function for timecop
+#'
+#'
+
+
+
+
 
 
