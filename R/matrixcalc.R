2af1828351a6ef90f004d4051769d7bebe3d55de
Christopher Crawford <47258177+crawfordcm@users.noreply.github.com>
2025-07-09 13:16:46 -0400

    Create matrixcalc.R

---
+++ R/matrixcalc.R
@@ -0,0 +1,259 @@
+#------------------------------------------------------------------------------#
+# matrix calc operators
+#------------------------------------------------------------------------------#
+# vec-type operators ====
+vec   <- function(x) t(t(as.vector(x))) # column vectorize matrix
+vech  <- function(x) t(t(x[!upper.tri(x)]))
+vecp  <- function(x) t(t(x[!upper.tri(x, diag = TRUE)]))
+vecd  <- function(x) t(t(diag(x)))
+
+# (reverse) vec-type operators ====
+
+revVec <- function(x, ncol, nrow){
+
+  if (length(x)==1)  { return(x) }
+
+  d <- sqrt(length(x))
+
+  if (missing(ncol) | missing(nrow)){
+    ncol <- nrow <- d
+    if (round(d) != d){
+      stop("MIIVsem: Dimensions needed for non-square matrices.")
+    }
+  }
+
+  revvecx <- matrix(0, nrow = nrow, ncol = ncol)
+
+  for (j in 1:ncol){
+    revvecx[,j] <- x[c(1:nrow) + (j-1)*nrow]
+  }
+  return(revvecx)
+}
+
+revVech <- function(x){
+
+  if (length(x)==1) { return(x) }
+
+  d <- (-1 + sqrt(8*length(x) + 1))/2
+
+  if (round(d) != d){
+    stop("MIIVsem: Matrix is not square.")
+  }
+
+  revvechx <- matrix(0, nrow=d, ncol=d)
+
+  for (j in 1:d){
+    revvechx[j:d,j] <- x[1:(d-j+1)+ (j-1)*(d - 1/2*(j-2))]
+  }
+
+  revvechx <- revvechx + t(revvechx) - diag(diag(revvechx))
+
+  return(revvechx)
+}
+
+revVecp <- function(x){
+
+  d <- (1 + sqrt(8*length(x) + 1))/2
+
+  if (round(d) != d){
+    stop("MIIVsem: Matrix is not square.")
+  }
+  revvecp <- matrix(0, nrow=d, ncol=d)
+  revvecp[lower.tri(revvecp , diag=FALSE)] <- x
+  diag(revvecp) <- 1
+  revvecp <- revvecp + t(revvecp) - diag(diag(revvecp))
+
+  return(revvecp)
+}
+
+# basis functions ====
+
+# DEF'N from Magnus & Neudecker (1980, p. 423): The unit
+#   vector e_i, i,...,n, is the ith column of the identity
+#   matrix I_n, or a vector length n with one in its
+#   ith position and zeros elsewhere.
+
+make_e_basis <- function(i, n) {
+
+  replace(numeric(n), i, 1)
+
+}
+
+# DEF'N from Magnus & Neudecker (1980, p. 423): The unit
+#   vector of length [1/2 * n * (n + 1)] where there is
+#   a one in the [(j-1) * n + i - .5*j*(j+1)] position
+#   and zeros elsewhere 1 <= j <= i <= n.
+
+make_u_basis <- function(i,j,n) {
+
+  replace(numeric(.5*n*(n+1)), (j-1)*n + i - .5*j*(j-1), 1)
+
+}
+
+# DEF'N from Neudecker (1983, p. 274): The unit
+#   vector of length [1/2 * n * (n - 1)] where there is
+#   a one in the [(j-1) * n + i - .5*j*(j+1)] position
+#   and zeros elsewhere 1 <= j < i <= n.
+
+make_v_basis <- function(i,j,n) {
+
+  replace(numeric(.5*n*(n-1)), (j-1)*n + i - .5*j*(j+1), 1)
+
+}
+
+# commutation matrix ====
+
+#  DEF'N from Magnus & Neudecker (1979, p. 383, 3.1a)
+comMat <- function(m, n=m){
+  K <- matrix(0, nrow = n*m, ncol = n*m)
+  for(i in 1:m){
+    for(j in 1:n){
+      H <- t(t(make_e_basis(i,m))) %*% t(make_e_basis(j,n))
+      K <- K + kronecker(H, t(H))
+    }
+  }
+  return(K)
+}
+
+
+# N Matrix ====
+
+nMat <- function(n){
+  return(.5 * (diag(n^2) + comMat(n)))
+}
+
+# L Matrix ====
+
+eliMat <- function(n, type = "s"){
+
+
+  if (type == "s"){
+
+    # case = "symmetric" (M vecA = vechA)
+    #  DEF'N from Magnus & Neudecker (1980, p. 425, 3.1b)
+    #  L in Magnus & Neudecker (1980)
+
+    M <- matrix(0, nrow= .5*n*(n+1), ncol = n^2)
+
+    for(i in 1:n){
+      for(j in 1:n){
+        if( i >= j){
+
+          M <- M + kronecker(kronecker(make_u_basis(i,j,n),t(make_e_basis(j,n))),t(make_e_basis(i,n)))
+
+        }
+      }
+    }
+
+  } else if (type == "l"){
+
+    # case = "strictly lower-triangular" (M vecA = vecpA)
+    #  DEF'N from Neudecker (1983, p. 276, 3.1b)
+    #  + L* in Neudecker (1983)
+
+    M <- matrix(0, nrow= .5*n*(n-1), ncol = n^2)
+
+    for(i in 1:n){
+      for(j in 1:n){
+        if( i > j){
+
+          M <- M + kronecker(kronecker(make_v_basis(i,j,n),t(make_e_basis(j,n))),t(make_e_basis(i,n)))
+
+        }
+      }
+    }
+
+  } else if (type == "d"){
+
+    # case = "diagonal" (M vecA = vecdA)
+    #  DEF'N from Neudecker (1983, p. 279, 3.3a)
+    #   + L** in Neudecker (1983)
+    M <- matrix(0, nrow = n, ncol = n^2)
+
+    for(i in 1:n){
+
+      M <- M + kronecker(kronecker(make_e_basis(i,n), t(make_e_basis(i,n))), t(make_e_basis(i,n)))
+
+    }
+
+  } else {
+
+    M <- NULL
+
+  }
+
+  return(M)
+
+}
+
+# duplication matrix ====
+
+# DEF'N from Magnus & Neudecker (1980, p. 428, 3.2b)
+dupMat <- function(n, type = "s"){
+
+  if (type == "s"){
+
+    # case = "symmetric" (D vechA = vecA)
+    #   DEF'N from Magnus & Neudecker (1980, p. 428, 3.2b)
+    #
+
+    Dt <- matrix(0, nrow = .5*n*(n+1), ncol= n^2)
+
+    for(i in 1:n){
+      for(j in 1:n){
+        if( i >= j){
+          Tm <- matrix(0,n,n); Tm[i,j] <- Tm[j,i] <- 1
+          Dt <- Dt + make_u_basis(i,j,n) %*% t(vec(Tm))
+        }
+      }
+    }
+
+    return(t(Dt))
+
+    # Neudecker (1983) Definition 3.2.a p. 278
+  } else if (type == "l"){
+
+    Dt <- matrix(0, nrow = .5*n*(n-1), ncol= n^2)
+
+    for(i in 1:n){
+      for(j in 1:n){
+        if( i > j){
+          T1 <- matrix(0,n,n); T1[i,j] <- 1
+          T2 <- matrix(0,n,n); T2[j,i] <- 1
+          Tm <- T1-T2
+          Dt <- Dt + make_v_basis(i,j,n) %*% t(vec(Tm))
+        }
+      }
+    }
+
+    return(t(Dt))
+
+    # Neudecker (1983) Definition 3.3.b Lemma 3.7 p. 280
+  } else if( type == "d"){
+
+    Dt <- eliMat(n, type = "d")
+    return(t(Dt))
+
+
+  }
+
+}
+
+
+
+makeCommutationMat <- function(n, m=n){
+
+  #  DEF'N from Magnus & Neudecker (1979, p. 383, 3.1a)
+
+  K <- matrix(0, nrow = n*m, ncol = n*m)
+
+  for(i in 1:m){
+    for(j in 1:n){
+
+      H <- t(t(make_e_basis(i,m))) %*% t(make_e_basis(j,n))
+      K <- K + kronecker(H, t(H))
+
+    }
+  }
+  return(t(K))
+}
