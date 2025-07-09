660b377888f5e5993cf1f1f8caad4f7eaaedc662
Christopher Crawford <47258177+crawfordcm@users.noreply.github.com>
2025-07-09 13:16:33 -0400

    Update latent_var_invlink.R

--- R/latent_var_invlink.R
+++ R/latent_var_invlink.R
@@ -6,7 +6,9 @@
 #' @param ell_ij_hat Array. An array of link function coefficients
 #' @keywords internal
 
-latent_var_invlink <- function(cov_x_hat, d, p, ell_ij_hat, grid_u){
+latent_var_invlink <- function(cov_x_hat, d, p, ell_ij_hat){
+
+  grid_u <- c(seq(-1,-0.71,0.01), seq(-0.7,0,0.2), seq(0.1,0.7,0.2), seq(0.71,1,0.01))
 
   cov_z_hat <- array(NA, dim(cov_x_hat))
   for (i in seq_len(d)) {
