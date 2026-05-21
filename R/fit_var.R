7628437821c431a01291aef95899ef8c04a43a90
Christopher Crawford <cmcrawford4792@gmail.com>
2026-05-20 21:24:32 -0400

    Add condition number warning before Yule-Walker solve
    Warn if Gamma_hat is ill-conditioned (kappa > 1e10) before calling
    solve(), so users are notified when estimates may be unreliable.

--- R/fit_var.R
+++ R/fit_var.R
@@ -11,6 +11,14 @@ fit_var <- function(gamma_hat, Gamma_hat, d) {
   if (!is.matrix(gamma_hat)) stop("'gamma_hat' must be a matrix", call. = FALSE)
   if (!is.matrix(Gamma_hat)) stop("'Gamma_hat' must be a matrix", call. = FALSE)
 
+  kappa_val <- kappa(Gamma_hat, exact = FALSE)
+  if (kappa_val > 1e10) {
+    warning(sprintf(
+      "[timecop] Gamma_hat is ill-conditioned (kappa = %.2e). Yule-Walker estimates may be unreliable.",
+      kappa_val
+    ), call. = FALSE)
+  }
+
   # get Yule-Walker estimates
   phi_hat <- t(solve(Gamma_hat) %*% t(gamma_hat))
 
